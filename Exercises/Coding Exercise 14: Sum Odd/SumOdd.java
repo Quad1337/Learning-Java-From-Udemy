c86f180a73953537265fa30ada261709066f9d27
Quad1337 <56504887+Quad1337@users.noreply.github.com>
2021-04-11 00:03:08 +0300

    Create SumOdd.java

---
+++ Exercises/Coding Exercise 14: Sum Odd/SumOdd.java
@@ -0,0 +1,24 @@
+public class SumOddRange {
+     public static boolean isOdd (int number){
+        
+        if(number <= 0){
+            return false;
+        }else if(number % 2 != 0 ){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    
+    public static int sumOdd (int start, int end){
+
+        if((start > end)||(start < 0 )||(end < 0))
+            return -1;
+        int sum = 0;
+        for(int i = start;i <= end; i++){
+            if (isOdd(i) == true)
+                sum = sum + i;
+        }
+        return sum;
+    }
+}
