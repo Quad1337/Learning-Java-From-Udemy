6c7737d7d08e1dd2ec8fcd2959fb49cef926f3e6
Quad1337 <56504887+Quad1337@users.noreply.github.com>
2021-04-10 23:58:45 +0300

    Create EqualSumChecker.java

---
+++ Exercises/Coding Exercise 6: Equal Sum Checker/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    
+    public static boolean hasEqualSum(int nu1, int nu2, int nu3){
+        if(nu1+nu2==nu3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+ 
+}
