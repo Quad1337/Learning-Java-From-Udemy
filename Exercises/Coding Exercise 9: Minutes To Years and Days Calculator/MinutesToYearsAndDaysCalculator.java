f79e2ef0651492d6e952951dc77f8bdd790f3db4
Quad1337 <56504887+Quad1337@users.noreply.github.com>
2021-04-11 00:00:26 +0300

    Create MinutesToYearsAndDaysCalculator.java

---
+++ Exercises/Coding Exercise 9: Minutes To Years and Days Calculator/MinutesToYearsAndDaysCalculator.java
@@ -0,0 +1,11 @@
+public class MinutesToYearsDaysCalculator {
+   public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            long year = minutes / (60 * 24 * 365);
+            long days = minutes / (60 * 24 ) - (year * 365);
+            System.out.println(minutes + " min = " + year + " y " + "and " + days + " d");
+        }
+    }
+}
